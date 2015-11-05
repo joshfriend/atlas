74b0674a438acd41614d9b620fe3f9061b614826
Josh Friend <jfriend@michiganlabs.com>
2015-11-05 02:18:43 -0500

    Add missing extensions file

---
+++ atlas/extensions.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Extensions module. Each extension is initialized in the app factory located
+in __init__.py
+"""
+
+from opbeat.contrib.flask import Opbeat
+opbeat = Opbeat()
